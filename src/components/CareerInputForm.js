a1ba6832debc48900516640456e282762a21f7d1
tylerlewisjcg <tylerlewisjcg@gmail.com>
2018-03-25 23:23:18 -0600

    Routing

---
+++ src/components/CareerInputForm.js
@@ -0,0 +1,1 @@
+////// this will have either the new education/training form or the new work history form shown in 'Career History' in my wireframe
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,26 @@
 import react from 'react';
 import { Route, Switch } from 'react-router-dom';
+import NotFound from './../components/NotFound';
+import Home from './../components/Home';
+import LoginPage from './../components/LoginPage';
+import SubscribePage from './../components/SubscribePage';
+import Achievements from './../components/Achievements';
+import CareerHistory from './../components/CareerHistory';
+import Motivations from './../components/Motivations';
 
 
 
 
 
-// export default (
-// <Switch>
-//     <Route exact path='/' component={}/>
-//     <Route path='/' component={}/>
-//     <Route path='/' component={}/>
-//     <Route path='/' component={}/>
-//     <Route path='/' component={}/>
-//     <Route path='/' component={}/>
-// </Switch>
+export default (
+<Switch>
+    <Route exact path='/' component={LoginPage}/>
+    <Route path='/subscribe' component={SubscribePage}/>
+    <Route path='/home' component={Home}/>
+    <Route path='/achievements' component={Achievements}/>
+    <Route path='/career' component={CareerHistory}/>
+    <Route path='/motivations' component={Motivations}/>
+    <Route component={NotFound}/>    
+</Switch>
 
-// )
+)
