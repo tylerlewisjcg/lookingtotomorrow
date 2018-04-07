39f77aa50eb996df67f48210e98c6d786f12a403
tylerlewisjcg <tylerlewisjcg@gmail.com>
2018-04-07 16:57:12 -0600

    got form submit working and returning

--- src/components/CareerHistory/CareerForm.js
+++ src/components/CareerHistory/CareerForm.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import {handleChange} from './../../ducks/workHistoryReducer';
+import {
+  handleCompanyChange,
+  handleEndDateChange,
+  handleJobResponsibilitiesChange,
+  handleJobTitleChange,
+  handleNotableAchievementsChange,
+  handleSalaryChange,
+  handleStartDateChange,
+  editWorkHistory
+} from "./../../ducks/workHistoryReducer";
+
+
+
 class CareerForm extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-
-  
 
   render() {
     return (
@@ -16,35 +26,35 @@ class CareerForm extends Component {
         <input
           name="company"
           placeholder={!!this.props.job ? this.props.job.company : ""}
-          onChange={e => this.props.handleChange(e)}
+          onChange={e => this.props.handleCompanyChange(e)}
         />
         <br />
         <span>Start Date</span>
         <input
           name="start_date"
           placeholder={!!this.props.job ? this.props.job.start_date : ""}
-          onChange={e => this.props.handleChange(e)}
+          onChange={e => this.props.handleStartDateChange(e)}
         />
         <br />
         <span>End Date</span>
         <input
           name="end_date"
           placeholder={!!this.props.job ? this.props.job.end_date : ""}
-          onChange={e => this.props.handleChange(e)}
+          onChange={e => this.props.handleEndDateChange(e)}
         />
         <br />
         <span>Salary</span>
         <input
           name="salary"
           placeholder={!!this.props.job ? this.props.job.salary : ""}
-          onChange={e => this.props.handleChange(e)}
+          onChange={e => this.props.handleSalaryChange(e)}
         />
         <br />
         <span>Job Title</span>
         <input
           name="job_title"
           placeholder={!!this.props.job ? this.props.job.job_title : ""}
-          onChange={e => this.props.handleChange(e)}
+          onChange={e => this.props.handleJobTitleChange(e)}
         />
         <br />
         <span>Job Responsibilities</span>
@@ -53,7 +63,7 @@ class CareerForm extends Component {
           placeholder={
             !!this.props.job ? this.props.job.job_responsibilities : ""
           }
-          onChange={e => this.props.handleChange(e)}
+          onChange={e => this.props.handleJobResponsibilitiesChange(e)}
         />
         <br />
         <span>Notable Achievements</span>
@@ -62,7 +72,7 @@ class CareerForm extends Component {
           placeholder={
             !!this.props.job ? this.props.job.notable_achievements : ""
           }
-          onChange={e => this.props.handleChange(e)}
+          onChange={e => this.props.handleNotableAchievementsChange(e)}
         />
         <br />
         <button
@@ -72,15 +82,37 @@ class CareerForm extends Component {
         >
           Cancel
         </button>
-        <button>Submit</button>
+        <button
+        onClick={()=> {
+      this.props.editWorkHistory(this.props.job.work_id, this.props.company, this.props.job_title, this.props.start_date, this.props.end_date, this.props.job_responsibilities, this.props.notable_achievements, this.props.salary)
+     }}
+        >Submit</button>
       </form>
     );
   }
 }
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
-      workHistoryItems: state.workHistory.workHistoryItems
-  }
-} 
+    workHistoryItems: state.workHistory.workHistoryItems,
+  company: state.workHistory.company,
+  job_title: state.workHistory.job_title,
+  start_date: state.workHistory.start_date,
+  end_date: state.workHistory.end_date,
+  job_responsibilities: state.workHistory.job_responsibilities,
+  notable_achievements: state.workHistory.notable_achievements,
+  salary: state.workHistory.salary
+  
+  };
+}
 
-export default connect(mapStateToProps, {handleChange})(CareerForm);
+export default connect(mapStateToProps, {
+  handleCompanyChange,
+  handleEndDateChange,
+  handleJobResponsibilitiesChange,
+  handleJobTitleChange,
+  handleJobTitleChange,
+  handleNotableAchievementsChange,
+  handleSalaryChange,
+  handleStartDateChange,
+  editWorkHistory
+})(CareerForm);
--- src/ducks/workHistoryReducer.js
+++ src/ducks/workHistoryReducer.js
@@ -18,6 +18,14 @@ import axios from "axios";
 const GET_WORK_HISTORY = "GET_WORK_HISTORY";
 const DELETE_WORK_HISTORY = "DELETE_WORK_HISTORY";
 const EDIT_WORK_HISTORY = "EDIT_WORK_HISTORY";
+const COMPANY_CHANGE = "COMPANY_CHANGE";
+const SALARY_CHANGE = "SALARY_CHANGE";
+const START_DATE_CHANGE = "START_DATE_CHANGE";
+const END_DATE_CHANGE = "END_DATE_CHANGE";
+const JOB_RESPONSIBILITIES_CHANGE = "JOB_RESPONSIBILITIES_CHANGE";
+const NOTABLE_ACHIEVEMENTS_CHANGE = "NOTABLE_ACHIEVEMENTS_CHANGE";
+const JOB_TITLE_CHANGE = "JOB_TITLE_CHANGE";
+
 
 ///// what i need to do.
 ///// i need my input fields in my careerform to change the state of company: "",
@@ -42,21 +50,60 @@ const EDIT_WORK_HISTORY = "EDIT_WORK_HISTORY";
 ////
 
 ///// trying to get handleChange to actually change values in the reducer state
-export function handleChange(e) {
-  console.log(
-    "this function needs to somehow be on my reducer editing global state to the value of the inputs of each form input box"
-  );
+export function handleCompanyChange(e) {
+  let newCompanyTemp = e.target.value
+  return {type: COMPANY_CHANGE,
+        payload: newCompanyTemp
+      }
+}
+export function handleStartDateChange(e) {
+  let newStartDateTemp = e.target.value
+  return {type: START_DATE_CHANGE,
+    payload: newStartDateTemp
+  }
+}
+export function handleEndDateChange(e){
+  let newEndDateTemp = e.target.value
+  return {type: END_DATE_CHANGE,
+    payload: newEndDateTemp
+  }
+}
+export function handleSalaryChange(e){
+  let newSalaryTemp = e.target.value
+  return {type: SALARY_CHANGE,
+    payload: newSalaryTemp
+  }
+}
+export function handleNotableAchievementsChange(e){
+  let newNotableAchievementsTemp = e.target.value
+  return {type: NOTABLE_ACHIEVEMENTS_CHANGE,
+    payload: newNotableAchievementsTemp
+  }
 }
 
-export function editWorkHistory( id, newCompany, newJobTitle, newStartDate, newEndDate, newJobResponsibilities, newNotableAchievements, newSalary ) {
+export function handleJobResponsibilitiesChange(e){
+  let newJobResponsibilitiesTemp = e.target.value
+  return {type: JOB_RESPONSIBILITIES_CHANGE,
+    payload: newJobResponsibilitiesTemp
+  }
+}
+
+export function handleJobTitleChange(e){
+  let newJobTitleTemp = e.target.value
+  return {type: JOB_TITLE_CHANGE,
+    payload: newJobTitleTemp
+  }
+}
+
+export function editWorkHistory( id, company, job_title, start_date, end_date, job_responsibilities, notable_achievements, salary ) {
   let body = {
-      company: newCompany,
-      job_title: newJobTitle,
-      start_date: newStartDate,
-      end_date: newEndDate,
-      job_responsibilities: newJobResponsibilities,
-      notable_achievements: newNotableAchievements,
-      salary: newSalary
+      company: company,
+      job_title: job_title,
+      start_date: start_date,
+      end_date: end_date,
+      job_responsibilities: job_responsibilities,
+      notable_achievements: notable_achievements,
+      salary: salary
   }
  const editData = axios.put( `/api/edit_work_history/${id}`, body )
   .then( response => {
@@ -101,8 +148,35 @@ export default function reducer(state = initialState, action) {
         case DELETE_WORK_HISTORY + "_FULFILLED":
         return Object.assign({}, state, {workHistoryItems: action.payload})
 
-        case EDIT_WORK_HISTORY  + "_FULFILLED":
+        case EDIT_WORK_HISTORY  + "_FULFILLED":   //// i don't think this is going to work
+        //because i don't have anything that is pushing the new values onto the workHistoryItems array
+        /// i might be able to pull down the info from initial state as props for every single key
+        //and then somehow take props.(key or whatever item) and push it onto a temporary array
+        //and then somehow make a new function that takes the value of that array from the component
+        // and sets it as the action.payload to push to workHistoryItems
         return Object.assign({}, state, {workHistoryItems: action.payload})
+
+        case COMPANY_CHANGE:
+        return Object.assign({}, state, {company: action.payload} )
+
+        case START_DATE_CHANGE:
+        return Object.assign({}, state, {start_date: action.payload} )
+
+        case END_DATE_CHANGE:
+        return Object.assign({}, state, {end_date: action.payload} )
+
+        case SALARY_CHANGE:
+        return Object.assign({}, state, {salary: action.payload} )
+
+        case JOB_RESPONSIBILITIES_CHANGE:
+        return Object.assign({}, state, {job_responsibilities: action.payload} )
+
+        case JOB_TITLE_CHANGE:
+        return Object.assign({}, state, {job_title: action.payload} )
+
+        case NOTABLE_ACHIEVEMENTS_CHANGE:
+        return Object.assign({}, state, {notable_achievements: action.payload} )
+
       default:
         return state;
     }
