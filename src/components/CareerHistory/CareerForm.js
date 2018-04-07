2a2e2e83287a20dcb01a3efd611e901d57eebeb0
tylerlewisjcg <tylerlewisjcg@gmail.com>
2018-04-07 13:36:46 -0600

    edit form populates correctly

--- src/components/CareerHistory/CareerForm.js
+++ src/components/CareerHistory/CareerForm.js
@@ -18,7 +18,7 @@ console.log('this function needs to somehow be on my reducer editing global stat
          <span>Company</span>
          <input
               name="company"
-             placeholder={!!this.props.job.company ? 
+             placeholder={!!this.props.job ? 
               this.props.job.company : ""}
             //  onChange={e => this.handleChange(e)}
             />
@@ -26,48 +26,48 @@ console.log('this function needs to somehow be on my reducer editing global stat
             <span>Start Date</span>
             <input
               name="start_date"
-            //  value={this.state.start_date}
-            //  placeholder={this.props.job.start_date}
+              placeholder={!!this.props.job ? 
+                this.props.job.start_date : ""}
             //  onChange={e => this.handleChange(e)}
             />
             <br />
             <span>End Date</span>
             <input
               name="end_date"
-            //  value={this.state.end_date}
-            //  placeholder={this.props.job.end_date}
+              placeholder={!!this.props.job ? 
+                this.props.job.end_date : ""}
             //  onChange={e => this.handleChange(e)}
             />
             <br />
             <span>Salary</span>
             <input
               name="salary"
-            //  value={this.state.salary}
-            //  placeholder={this.props.job.salary}
+              placeholder={!!this.props.job ? 
+                this.props.job.salary : ""}
             //  onChange={e => this.handleChange(e)}
             />
             <br />
             <span>Job Title</span>
             <input
               name="job_title"
-            //  value={this.state.job_title}
-            //  placeholder={this.props.job.job_title}
+              placeholder={!!this.props.job ? 
+                this.props.job.job_title : ""}
             //  onChange={e => this.handleChange(e)}
             />
             <br />
             <span>Job Responsibilities</span>
             <input
               name="job_responsibilities"
-            //  value={this.state.job_responsibilities}
-            //  placeholder={this.props.job.job_responsibilities}
+              placeholder={!!this.props.job ? 
+                this.props.job.job_responsibilities : ""}
             //  onChange={e => this.handleChange(e)}
             />
             <br />
             <span>Notable Achievements</span>
             <input
               name="notable_achievements"
-            //  value={this.state.notable_achievements}
-            //  placeholder={this.props.job.notable_achievements}
+              placeholder={!!this.props.job ? 
+                this.props.job.notable_achievements : ""}
             //  onChange={e => this.handleChange(e)}
             />
             <br />
