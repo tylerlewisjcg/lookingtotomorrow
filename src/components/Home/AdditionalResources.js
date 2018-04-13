b33124042a34e373ac78a4edb5f20f8baddc8aaa
tylerlewisjcg <tylerlewisjcg@gmail.com>
2018-04-13 13:21:09 -0600

    added additional resource links

--- src/components/Home/AdditionalResources.js
+++ src/components/Home/AdditionalResources.js
@@ -2,7 +2,18 @@ import React from 'react';
 const AdditionalResources = () => {
     return (
         <div>
-            AdditionalResources Component
+            AdditionalResources
+            <a href="https://www.devmountain.com"><p>DevMountain</p></a>
+            <a href="https://www.codecademy.com"><p>Codecademy</p></a>
+            <a href="https://www.khanacademy.org"><p>KhanAcademy</p></a>
+            <a href="https://www.udemy.com"><p>Udemy</p></a>
+            <a href="https://www.coursera.org"><p>Coursera</p></a>
+            <a href="https://www.ed2go.com"><p>ed2go</p></a>
+            <a href="https://www.duolingo.com"><p>DuoLingo</p></a>
+            <a href="https://www.memrise.com"><p>memrise</p></a>
+            <a href="https://www.edx.org"><p>edX</p></a>
+            <a href="https://www.alison.com"><p>Alison</p></a>
+            
         </div>
     )
 }
